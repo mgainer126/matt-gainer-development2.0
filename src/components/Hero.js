19f08d33c5873eecf260cf014e1bba4cb581abee
Matthew Gainer <matthewgainer@matthewacstudio.lan>
2023-06-23 20:03:40 -0400

    updated to have a favicon the tab label as well as a logo instead of a name on the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,28 @@
 import ParticlesBg from "particles-bg";
 import github from "../assets/images/github.svg";
 import data from "../assets/data/resumeData.json";
+import logo from "../assets/images/logo.png";
 
-
-function Hero (){
-
-
-    return(
-        <>
-      
-        <section className="hero">
+function Hero() {
+  return (
+    <>
+      <section className="hero">
         <ParticlesBg color="random" type="square" bg={true} />
-        
-       
-            <h1 className="hero-title">
-                {data.main.name}
-            </h1>
-            <p className="hero-description">
-                {data.main.description}
-            </p>
-            <a href={data.main.github} className="hero-link">
-            <div className="hero-btn">
-              <img src={github} alt="github" className="hero-btn-icon"/>
-              <p className="hero-github">GitHub</p>
+
+        {/* <h1 className="hero-title">{data.main.name}</h1> */}
+
+        <p className="hero-description">
+          <img src={logo} alt="logo" className="hero-logo" />
+        </p>
+        <a href={data.main.github} className="hero-link">
+          <div className="hero-btn">
+            <img src={github} alt="github" className="hero-btn-icon" />
+            <p className="hero-github">GitHub</p>
           </div>
-            </a>
-            
-        </section>
-        
-        </>
-        
-    )
+        </a>
+      </section>
+    </>
+  );
 }
 
 export default Hero;
