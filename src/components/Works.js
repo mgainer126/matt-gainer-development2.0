d75aeab6f893ebff6f285ea94fef8841569f9143
Matt <inquire@mattgainerdevelopment.com>
2023-07-25 16:32:19 -0400

    add progear180 to portfolio

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,18 +1,19 @@
 import data from "../assets/data/resumeData.json";
-import portfolio from '../assets/images/01.png';
+import portfolio from "../assets/images/01.png";
 // import dashboard from '../assets/images/02.png';
-import foodiofare from '../assets/images/03.png';
-import brewercave from '../assets/images/04.png'
-import ecommerce from '../assets/images/05.png'
-import React from 'react';
+import foodiofare from "../assets/images/03.png";
+import brewercave from "../assets/images/04.png";
+import ecommerce from "../assets/images/05.png";
+import progear180 from "../assets/images/06.png";
+import React from "react";
 
-function Works (){
+function Works() {
   const [isVisible, setVisible] = React.useState(true);
   const domRef = React.useRef();
 
   React.useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => setVisible(entry.isIntersecting));
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
     });
 
     const currentRef = domRef.current; // Create a local variable
@@ -27,40 +28,58 @@ function Works (){
       }
     };
   }, []);
-    return(
-        <section className="works">
-                  <div className={`fade-in-left ${isVisible ? 'is-visible' : ''}`}
-        ref={domRef}>
+  return (
+    <section className="works">
+      <div
+        className={`fade-in-left ${isVisible ? "is-visible" : ""}`}
+        ref={domRef}
+      >
         <h2 className="works-title">CHECK OUT SOME OF MY WORKS</h2>
-           <div className="works-list">
-            <a href="https://www.mattgainerdevelopment.com/" >
-              <div className="works-item" >
-                <img src={portfolio} alt="project" className="works-item-img"/>
-                <div className="works-item-title">{data.portfolio.projects[0].title}</div>
+        <div className="works-list">
+          <a href="https://www.mattgainerdevelopment.com/">
+            <div className="works-item">
+              <img src={portfolio} alt="project" className="works-item-img" />
+              <div className="works-item-title">
+                {data.portfolio.projects[0].title}
               </div>
-            </a>
-            <a href="https://www.foodiofare.com/" >
-            <div className="works-item" >
-              <img src={foodiofare} alt="project" className="works-item-img"/>
-              <div className="works-item-title">{data.portfolio.projects[2].title}</div>
             </div>
-            </a>
-            <a href="https://www.brewercave.com/" >
-            <div className="works-item" >
-              <img src={brewercave} alt="project" className="works-item-img"/>
-              <div className="works-item-title">{data.portfolio.projects[3].title}</div>
+          </a>
+          <a href="https://www.foodiofare.com/">
+            <div className="works-item">
+              <img src={foodiofare} alt="project" className="works-item-img" />
+              <div className="works-item-title">
+                {data.portfolio.projects[2].title}
+              </div>
             </div>
-            </a>
-            <a href="https://coolhunterz.netlify.app/" >
-            <div className="works-item" >
-              <img src={ecommerce} alt="project" className="works-item-img"/>
-              <div className="works-item-title">{data.portfolio.projects[4].title}</div>
+          </a>
+          <a href="https://www.brewercave.com/">
+            <div className="works-item">
+              <img src={brewercave} alt="project" className="works-item-img" />
+              <div className="works-item-title">
+                {data.portfolio.projects[3].title}
+              </div>
             </div>
-            </a>
-</div>
-</div>
+          </a>
+          <a href="https://coolhunterz.netlify.app/">
+            <div className="works-item">
+              <img src={ecommerce} alt="project" className="works-item-img" />
+              <div className="works-item-title">
+                {data.portfolio.projects[4].title}
+              </div>
+            </div>
+          </a>
+          <a href="https://progear180.com/">
+            <div className="works-item">
+              <img src={progear180} alt="project" className="works-item-img" />
+              <div className="works-item-title">
+                {data.portfolio.projects[4].title}
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </section>
-    );
+  );
 }
 
 export default Works;
