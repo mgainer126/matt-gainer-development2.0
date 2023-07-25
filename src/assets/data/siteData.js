69eac81d915efe9e1b9172d5c394c6875d02e62f
Matt <inquire@mattgainerdevelopment.com>
2023-07-25 17:11:10 -0400

    refactor site to use a JS file

---
+++ src/assets/data/siteData.js
@@ -0,0 +1,41 @@
+import image1 from "..//images//01.png";
+
+import image3 from "..//images//03.png";
+import image4 from "..//images//04.png";
+import image5 from "..//images//05.png";
+import image6 from "..//images//06.png";
+
+const projects = [
+  {
+    title: "Resume Website",
+    category: "Work",
+    image: image1,
+    url: "https://www.mattgainerdevelopment.com/",
+  },
+  {
+    title: "Foodio Fare",
+    category: "Food",
+    image: image3,
+    url: "https://www.foodiofare.com/",
+  },
+  {
+    title: "Brewer Cave",
+    category: "Food",
+    image: image4,
+    url: "https://www.brewercave.com/",
+  },
+  {
+    title: "Cool Hunterz",
+    category: "eCommerce",
+    image: image5,
+    url: "https://coolhunterz.netlify.app/",
+  },
+  {
+    title: "ProGear 180",
+    category: "eCommerce",
+    image: image6,
+    url: "https://progear180.com/",
+  },
+];
+
+export { projects };
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,13 +1,8 @@
-import data from "../assets/data/resumeData.json";
-import portfolio from "../assets/images/01.png";
-// import dashboard from '../assets/images/02.png';
-import foodiofare from "../assets/images/03.png";
-import brewercave from "../assets/images/04.png";
-import ecommerce from "../assets/images/05.png";
-import progear180 from "../assets/images/06.png";
 import React from "react";
+import { projects } from "../assets/data/siteData.js";
 
 function Works() {
+  console.log(projects);
   const [isVisible, setVisible] = React.useState(true);
   const domRef = React.useRef();
 
@@ -16,7 +11,7 @@ function Works() {
       entries.forEach((entry) => setVisible(entry.isIntersecting));
     });
 
-    const currentRef = domRef.current; // Create a local variable
+    const currentRef = domRef.current;
 
     if (currentRef) {
       observer.observe(currentRef);
@@ -36,46 +31,22 @@ function Works() {
       >
         <h2 className="works-title">CHECK OUT SOME OF MY WORKS</h2>
         <div className="works-list">
-          <a href="https://www.mattgainerdevelopment.com/">
-            <div className="works-item">
-              <img src={portfolio} alt="project" className="works-item-img" />
-              <div className="works-item-title">
-                {data.portfolio.projects[0].title}
-              </div>
-            </div>
-          </a>
-          <a href="https://www.foodiofare.com/">
-            <div className="works-item">
-              <img src={foodiofare} alt="project" className="works-item-img" />
-              <div className="works-item-title">
-                {data.portfolio.projects[2].title}
-              </div>
-            </div>
-          </a>
-          <a href="https://www.brewercave.com/">
-            <div className="works-item">
-              <img src={brewercave} alt="project" className="works-item-img" />
-              <div className="works-item-title">
-                {data.portfolio.projects[3].title}
-              </div>
-            </div>
-          </a>
-          <a href="https://coolhunterz.netlify.app/">
-            <div className="works-item">
-              <img src={ecommerce} alt="project" className="works-item-img" />
-              <div className="works-item-title">
-                {data.portfolio.projects[4].title}
-              </div>
-            </div>
-          </a>
-          <a href="https://progear180.com/">
-            <div className="works-item">
-              <img src={progear180} alt="project" className="works-item-img" />
-              <div className="works-item-title">
-                {data.portfolio.projects[5].title}
-              </div>
-            </div>
-          </a>
+          {projects.map((project, index) => {
+            return (
+              <>
+                <a href={project.url} key={index}>
+                  <div className="works-item">
+                    <img
+                      src={project.image}
+                      alt="project"
+                      className="works-item-img"
+                    />
+                    <div className="works-item-title">{project.title}</div>
+                  </div>
+                </a>
+              </>
+            );
+          })}
         </div>
       </div>
     </section>
