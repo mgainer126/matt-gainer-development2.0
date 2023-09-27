9f6414740b9411356741e3697b30083b740e782e
Matt <inquire@mattgainerdevelopment.com>
2023-09-27 16:11:17 -0400

    added life pure essentials

--- src/assets/data/siteData.js
+++ src/assets/data/siteData.js
@@ -4,6 +4,7 @@ import image3 from "..//images//03.png";
 import image4 from "..//images//04.png";
 import image5 from "..//images//05.png";
 import image6 from "..//images//06.png";
+import image7 from "..//images//07.png";
 
 const projects = [
   {
@@ -36,6 +37,88 @@ const projects = [
     image: image6,
     url: "https://progear180.com/",
   },
+  {
+    title: "Life Pure Essentials",
+    category: "eCommerce",
+    image: image7,
+    url: "https://lifepureessentials.com/",
+  },
 ];
 
-export { projects };
+const skills = [
+  {
+    name: "REACTJS",
+    level: "95",
+  },
+  {
+    name: "JAVASCRIPT",
+    level: "90",
+  },
+  {
+    name: "SCSS",
+    level: "90",
+  },
+  {
+    name: "CSS",
+    level: "90",
+  },
+  {
+    name: "WEBAPI",
+    level: "90",
+  },
+
+  {
+    name: "KNEX",
+    level: "80",
+  },
+  {
+    name: "NODEJS",
+    level: "80",
+  },
+  {
+    name: "EXPRESS",
+    level: "80",
+  },
+  {
+    name: "DOM",
+    level: "90",
+  },
+  {
+    name: "AGILE",
+    level: "100",
+  },
+  {
+    name: "JIRA",
+    level: "100",
+  },
+  {
+    name: "CONFLUENCE",
+    level: "100",
+  },
+  {
+    name: "TS2 (TSYS)",
+    level: "100",
+  },
+  {
+    name: "MYSQL",
+    level: "80",
+  },
+  {
+    name: "BUSINESS PROCESS MODELING",
+    level: "100",
+  },
+  {
+    name: "BUSINESS PROCESS MODELING",
+    level: "100",
+  },
+  {
+    name: "BUSINESS CASE MANAGEMENT",
+    level: "100",
+  },
+  {
+    name: "REQUIREMENTS GATHERING AND ANALYSIS",
+    level: "100",
+  },
+];
+
+export { projects, skills };
